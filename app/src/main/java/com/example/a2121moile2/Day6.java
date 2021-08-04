df458924629a05bda12303161bc1baca6a22000d
WIN-OF200G3MJ2E\user <medngueu@gmail.com>
2021-08-04 16:38:37 +0300

    Заполнены все дни. Не реализован переход по неделям.

---
+++ app/src/main/java/com/example/a2121moile2/Day6.java
@@ -0,0 +1,11 @@
+package com.example.a2121moile2;
+
+import android.os.Bundle;
+
+public class Day6 extends MainActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.day6);
+    }
+}
--- app/src/main/java/com/example/a2121moile2/MainActivity.java
+++ app/src/main/java/com/example/a2121moile2/MainActivity.java
@@ -16,6 +16,12 @@ public class MainActivity extends AppCompatActivity {
 
         Button button1= (Button) findViewById(R.id.button1);
         Button button2= (Button) findViewById(R.id.button2);
+        Button button3= (Button) findViewById(R.id.button3);
+        Button button4= (Button) findViewById(R.id.button4);
+        Button button5= (Button) findViewById(R.id.button5);
+        Button button6= (Button) findViewById(R.id.button6);
+
+
 
         button1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -26,6 +32,24 @@ public class MainActivity extends AppCompatActivity {
         button2.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, Day2.class));
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Day3.class));
+            }
+        });
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Day4.class));
+            }
+        });
+
+        button6.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Day6.class));
             }
         });
     }
