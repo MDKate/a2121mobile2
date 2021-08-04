66344c239482d59d5c9d9a3ac6b5d22e3d80df63
WIN-OF200G3MJ2E\user <medngueu@gmail.com>
2021-08-04 15:09:46 +0300

    Пивот.

---
+++ app/src/main/java/com/example/a2121moile2/Day.java
@@ -0,0 +1,13 @@
+package com.example.a2121moile2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Day extends MainActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.day);
+    }
+}
